1f0402ca9cbb63c631c3fc887bb79431ddfb005f
bormolei <Oxitxmq5q@>
2021-05-04 09:11:11 +0300

    Test URI tickets

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -14,6 +14,7 @@ import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 import org.telegram.telegrambots.meta.generics.BotSession;
 import service.HibernateService.BotCalendarService;
+import service.Tickets.TicketsMain;
 
 import java.io.InputStreamReader;
 import java.text.ParseException;
@@ -43,53 +44,9 @@ public class main {
 
     //2021-03-27 11:56:13.445000
     private static void test() throws ParseException {
-        String str = "https://api.rasp.yandex.net/v3.0/stations_list/?apikey=1324d008-778c-4fca-a057-2c7ce97c7b92&lang=ru_RU&format=json";
-        Response response = RestAssured.given()
-                .contentType(ContentType.JSON)
-                .get(str);
-        JsonArray s = new JsonParser()
-                .parse(new InputStreamReader(response.asInputStream()))
-                .getAsJsonObject().get("countries")
-                .getAsJsonArray();
-//                .get(200)//При необходимости сделать цикл
-//                .getAsJsonObject();
-//                .get("text")
-//                .toString()
-//                .replace("\"","");
-        String myCity = "Москва";
-        s.get(0);
-        Map<Integer, String> yandexRasp = new HashMap<>();
-//        for (int i = 0; i < s.size(); i++) {
-//            String country = s.get(i).getAsJsonObject().get("title").toString();
-//            yandexRasp.put(i, country);
-//        }
-//        yandexRasp.entrySet().forEach(System.out::println);
-        String country;
-        String region;
-        Map<String, String> l = new HashMap();
-        List tset = new ArrayList();
-        JsonArray regions;
-        JsonArray settlements;
-        JsonObject city;
-        for (int i = 0; i < s.size(); i++) {
-            country = s.get(i).getAsJsonObject().get("title").toString().replace("\"","");
-            regions = s.get(i).getAsJsonObject().getAsJsonArray("regions");
-            for (int j = 0; j < regions.size(); j++) {
-                region = regions.get(j).getAsJsonObject().get("title").toString().replace("\"", "");
-                settlements = regions.get(j).getAsJsonObject().getAsJsonArray("settlements");
-                for (int k = 0; k < settlements.size(); k++) {
-                    city = settlements.get(k).getAsJsonObject();
-                    String cityName = city.get("title").toString().replace("\"", "");
-                    if (cityName.equals(myCity)) {
-                        System.out.println(country + " " + region + " " + cityName + " " + city.getAsJsonObject("codes").get("yandex_code").toString().replace("\"",""));
-                    }
-                    ; //ДОДЕЛАТЬ
-                }
-
-            }
-
-
-        }
+        String from = TicketsMain.getTicketInfo("Кострома");
+        String to = TicketsMain.getTicketInfo("Москва");
+        TicketsMain.getWay(from, to,"");
         System.out.println("finish");
 //        JsonArray json = new JsonArray(response);
 //        Long l = 1123699229L;
